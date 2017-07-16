90cbba33813241a94492c26ad3b0e6f2975ce9be
leomenbel <leomenbel93@gmail.com>
2017-07-16 15:05:17 -0500

    Adding all the project files

---
+++ desktop/src/com/leomenbel/offthewall/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.leomenbel.offthewall.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.leomenbel.offthewall.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
